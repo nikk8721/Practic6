4b89ead0ba66d420d7985db1906adf8713f0f9b5
nikk8721 <nikk8721@gmail.com>
2022-07-18 20:25:40 +0500

    Task41

---
+++ Task41/Program.cs
@@ -0,0 +1,14 @@
+﻿// Ввод M чисел с клавиатуры. Подсчитать сколько чисел больше 0
+Console.Write("Введите элементы(через пробел): ");
+int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse); 
+int count = 0;
+ 
+for (int i = 0; i < arr.Length; i++)
+{
+    if (arr[i] > 0)
+    {
+        count++;
+    }
+}
+ 
+Console.WriteLine($"Кол-во элементов > 0: {count}");
